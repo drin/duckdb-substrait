ad2b48e2ba132576e2139acc9e8e66514bec8ca2
Aldrin Montana <octalene.dev@pm.me>
2024-03-05 16:10:28 -0800

    task: reorganized substrait code
    This reorganization is partially for my own benefit (readability and
    internalizing the structure), though I did add some improvements such as
    how substrait types are used in switch statements.
    
    The other motivation for this reorganization is so that I can have a
    better overall structure/framework for also "translating" a substrait
    plan to a logical duckdb plan (instead of only a physical duckdb plan).
    
    In this code, I use "translation" to refer to a _lowering_ from a
    logical substrait plan to a physical duckdb plan.

---
+++ src/engine_duckdb.cpp
@@ -0,0 +1,132 @@
+// ------------------------------
+// License
+//
+// Copyright 2024 Aldrin Montana
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+// ------------------------------
+// Dependencies
+
+#include "engine_duckdb.hpp"
+
+
+// ------------------------------
+// Functions
+
+// >> Utility functions
+
+namespace duckdb {
+
+  //! Set static lookup table for substrait -> duckdb function names
+  const std::unordered_map<std::string, std::string>
+  DuckDBEnginePlan::function_names_remap = {
+     {"modulus"    , "mod"      }
+    ,{"std_dev"    , "stddev"   }
+    ,{"starts_with", "prefix"   }
+    ,{"ends_with"  , "suffix"   }
+    ,{"substring"  , "substr"   }
+    ,{"char_length", "length"   }
+    ,{"is_nan"     , "isnan"    }
+    ,{"is_finite"  , "isfinite" }
+    ,{"is_infinite", "isinf"    }
+    ,{"like"       , "~~"       }
+    ,{"extract"    , "date_part"}
+  };
+
+  //! Set static reference table for duckdb date subfields
+  const case_insensitive_set_t
+  DuckDBEnginePlan::valid_extract_subfields = {
+     "year"
+    ,"month"
+    ,"day"
+    ,"decade"
+    ,"century"
+    ,"millenium"
+    ,"quarter"
+    ,"microsecond"
+    ,"milliseconds"
+    ,"second"
+    ,"minute"
+    , "hour"
+  };
+
+
+  // >> DuckDBEnginePlan
+  DuckDBEnginePlan::DuckDBEnginePlan(Connection& conn): conn(conn) {}
+
+  void DuckDBEnginePlan::RegisterExtensionFunctions(substrait::Plan& plan) {
+    for (auto &sext : plan.extensions()) {
+      if (!sext.has_extension_function()) { continue; }
+
+      const auto fn_anchor     = sext.extension_function().function_anchor();
+      functions_map[fn_anchor] = sext.extension_function().name();
+    }
+  }
+
+  void DuckDBEnginePlan::VerifyCorrectExtractSubfield(const string& subfield) {
+    D_ASSERT(DuckDBEnginePlan::valid_extract_subfields.count(subfield));
+  }
+
+  string DuckDBEnginePlan::RemoveExtension(string &function_name) {
+    // Lets first drop any extension id
+    string name;
+    for (auto &c : function_name) {
+      if (c == ':') { break; }
+      name += c;
+    }
+
+    return name;
+  }
+
+  string DuckDBEnginePlan::RemapFunctionName(string &function_name) {
+    string name { RemoveExtension(function_name) };
+
+    auto it = function_names_remap.find(name);
+    if (it != function_names_remap.end()) { name = it->second; }
+
+    return name;
+  }
+
+  string DuckDBEnginePlan::FindFunction(uint64_t id) {
+    if (functions_map.find(id) == functions_map.end()) {
+      throw InternalException("Could not find aggregate function " + to_string(id));
+    }
+
+    return functions_map[id];
+  }
+
+  shared_ptr<LogicalOperator>
+  DuckDBEnginePlan::SystemPlanFromSubstraitPlan(substrait::Plan& plan) {
+    if (plan.relations().empty()) {
+      throw InvalidInputException("Substrait Plan does not have a SELECT statement");
+    }
+
+    RegisterExtensionFunctions(plan);
+
+    return TranspileRootOp(plan.relations(0).root());
+  }
+
+  shared_ptr<Relation>
+  DuckDBEnginePlan::EnginePlanFromSubstraitPlan(substrait::Plan& plan) {
+    if (plan.relations().empty()) {
+      throw InvalidInputException("Substrait Plan does not have a SELECT statement");
+    }
+
+    RegisterExtensionFunctions(plan);
+
+    return TranslateRootOp(plan.relations(0).root());
+  }
+
+} // namespace duckdb
---
+++ src/include/plans.hpp
@@ -0,0 +1,181 @@
+// ------------------------------
+// License
+//
+// Copyright 2024 Aldrin Montana
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+// ------------------------------
+// Dependencies
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <iostream> // for debugging
+
+#include "duckdb/main/connection.hpp"
+#include "duckdb/main/client_data.hpp"
+
+#include "duckdb/parser/parser.hpp"
+
+#include "duckdb/common/exception.hpp"
+#include "duckdb/common/helper.hpp"
+#include "duckdb/common/http_state.hpp"
+#include "duckdb/common/types.hpp"
+#include "duckdb/common/types/value.hpp"
+#include "duckdb/common/enums/set_operation_type.hpp"
+
+#include "substrait/plan.pb.h"
+#include "substrait/algebra.pb.h"
+
+
+// ------------------------------
+// Macros and Type Aliases
+
+// Standard types
+using std::string;
+using std::unordered_map;
+
+// Convenience aliases
+using FunctionMap = unordered_map<uint64_t, string>;
+
+
+namespace duckdb {
+
+  //! Execution plan (physical plan) for DuckDB
+  struct DuckDBEnginePlan {
+    // >> Constructors
+
+    //! Initializes a Translator instance
+    DuckDBEnginePlan(Connection& conn);
+
+
+    // >> Convenience functions
+
+    //! Reusable function that updates function extensions given a substrait plan
+    void RegisterExtensionFunctions(substrait::Plan& plan);
+
+    //! Check that the subfield is supported by duckdb
+    void VerifyCorrectExtractSubfield(const string& subfield);
+
+    //! Normalize function name by dropping extension ID
+    string RemoveExtension(string& function_name);
+
+    //! Remap substrait function name to duckdb function name
+    string RemapFunctionName(string& function_name);
+
+    //! Looks up for aggregation function in functions_map
+    string FindFunction(uint64_t id);
+
+
+    // ------------------------------
+    // Transpilation functions (substrait -> logical)
+
+    //! Transpile a substrait plan to a duckdb execution plan
+    shared_ptr<LogicalOperator> SystemPlanFromSubstraitPlan(substrait::Plan& plan);
+
+    //! Transpile a substrait plan to a duckdb execution plan
+    shared_ptr<Relation> TranspileRootOp(const substrait::RelRoot& sop);
+
+    //! Transpile a substrait operator to a duckdb execution operator
+    shared_ptr<Relation> TranspileOp(const substrait::Rel& sop);
+
+    //! Transpile a substrait expression to a duckdb expression
+    unique_ptr<ParsedExpression> TranspileExpr(const substrait::Expression &sexpr);
+
+
+    // ------------------------------
+    // Translation functions (substrait -> physical)
+
+    //! Translate a substrait plan to a duckdb execution plan
+    shared_ptr<Relation> EnginePlanFromSubstraitPlan(substrait::Plan& plan);
+
+    //! Translate a substrait plan to a duckdb execution plan
+    shared_ptr<Relation> TranslateRootOp(const substrait::RelRoot& sop);
+
+    //! Translate a substrait operator to a duckdb execution operator
+    shared_ptr<Relation> TranslateOp(const substrait::Rel& sop);
+
+    //! Translate a substrait expression to a duckdb expression
+    unique_ptr<ParsedExpression> TranslateExpr(const substrait::Expression &sexpr);
+
+    // >> Internal translation functions for operators
+    // NOTE: these are member methods because TranslateReadOp uses this->conn
+    shared_ptr<Relation> TranslateJoinOp         (const substrait::JoinRel&      sjoin);
+    shared_ptr<Relation> TranslateCrossProductOp (const substrait::CrossRel&     scross);
+    shared_ptr<Relation> TranslateFetchOp        (const substrait::FetchRel&     slimit);
+    shared_ptr<Relation> TranslateFilterOp       (const substrait::FilterRel&    sfilter);
+    shared_ptr<Relation> TranslateProjectOp      (const substrait::ProjectRel&   sproj);
+    shared_ptr<Relation> TranslateAggregateOp    (const substrait::AggregateRel& saggr);
+    shared_ptr<Relation> TranslateReadOp         (const substrait::ReadRel&      sget);
+    shared_ptr<Relation> TranslateSortOp         (const substrait::SortRel&      ssort);
+    shared_ptr<Relation> TranslateSetOp          (const substrait::SetRel&       sset);
+
+    //! Translate Substrait Sort Order to DuckDB Order
+    OrderByNode TranslateOrder(const substrait::SortField& sordf);
+
+
+    // >> Internal translation functions for expressions
+    unique_ptr<ParsedExpression> TranslateSelectionExpr(const substrait::Expression& sexpr);
+    unique_ptr<ParsedExpression> TranslateIfThenExpr   (const substrait::Expression& sexpr);
+    unique_ptr<ParsedExpression> TranslateCastExpr     (const substrait::Expression& sexpr);
+    unique_ptr<ParsedExpression> TranslateInExpr       (const substrait::Expression& sexpr);
+
+    unique_ptr<ParsedExpression>
+    TranslateLiteralExpr(const substrait::Expression::Literal& slit);
+
+    unique_ptr<ParsedExpression>
+    TranslateScalarFunctionExpr(const substrait::Expression& sexpr);
+
+
+    // >> Member variables
+    private:
+      //! DuckDB `Connection`
+      Connection&  conn;
+
+      //! Map of registered substrait function extensions
+      FunctionMap functions_map;
+
+      //! Remap substrait function names to DuckDB function names
+      static const unordered_map<string, string> function_names_remap;
+      static const case_insensitive_set_t        valid_extract_subfields;
+  };
+
+  //! Translator from substrait plan to DuckDB plans
+  struct DuckDBTranslator {
+    //! Initializes a Translator instance
+    DuckDBTranslator(ClientContext& context);
+
+    //! Transforms serialized Substrait Plan to DuckDB Relation
+    shared_ptr<LogicalOperator> TranspilePlanMessage(const string& serialized_msg);
+
+    //! Transforms serialized Substrait Plan to DuckDB Relation
+    shared_ptr<Relation> TranslatePlanMessage(const string& serialized_msg);
+
+    //! Transforms json-formatted Substrait Plan to DuckDB Relation
+    shared_ptr<Relation> TranslatePlanJson(const string& json_msg);
+
+    private:
+      //! DuckDB `ClientContext` inherited from initial DuckDB Connection
+      ClientContext &context;
+
+      //! DuckDB `Connection` initiated by `DuckDBTranslator`
+      unique_ptr<Connection> t_conn;
+
+      //! Substrait Plan
+      substrait::Plan plan;
+  };
+
+} // namespace duckdb
