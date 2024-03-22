5ed96473f51170aa4be2356d1732545432e6adb7
Aldrin Montana <octalene.dev@pm.me>
2024-03-22 16:09:33 -0700

    task: moved engine specific code to engine_duckdb
    this way, plans.hpp and plans.cpp can provide a structure for
    engine-agnostic hooks (though it doesn't seem very good right now)

--- src/engine_duckdb.cpp
+++ src/engine_duckdb.cpp
@@ -25,13 +25,13 @@
 // ------------------------------
 // Functions
 
-// >> Utility functions
+// >> DuckDB-specific function renaming and validation
 
 namespace duckdb {
 
-  //! Set static lookup table for substrait -> duckdb function names
-  const std::unordered_map<std::string, std::string>
-  DuckDBEnginePlan::function_names_remap = {
+  // >> Static data and related functions for mapping functions from substrait -> duckdb
+
+  static FunctionRenameMap engine_remapped_functions {
      {"modulus"    , "mod"      }
     ,{"std_dev"    , "stddev"   }
     ,{"starts_with", "prefix"   }
@@ -45,43 +45,9 @@ namespace duckdb {
     ,{"extract"    , "date_part"}
   };
 
-  //! Set static reference table for duckdb date subfields
-  const case_insensitive_set_t
-  DuckDBEnginePlan::valid_extract_subfields = {
-     "year"
-    ,"month"
-    ,"day"
-    ,"decade"
-    ,"century"
-    ,"millenium"
-    ,"quarter"
-    ,"microsecond"
-    ,"milliseconds"
-    ,"second"
-    ,"minute"
-    , "hour"
-  };
-
-
-  // >> DuckDBEnginePlan
-  DuckDBEnginePlan::DuckDBEnginePlan(Connection& conn): conn(conn) {}
-
-  void DuckDBEnginePlan::RegisterExtensionFunctions(substrait::Plan& plan) {
-    for (auto &sext : plan.extensions()) {
-      if (!sext.has_extension_function()) { continue; }
-
-      const auto fn_anchor     = sext.extension_function().function_anchor();
-      functions_map[fn_anchor] = sext.extension_function().name();
-    }
-  }
-
-  void DuckDBEnginePlan::VerifyCorrectExtractSubfield(const string& subfield) {
-    D_ASSERT(DuckDBEnginePlan::valid_extract_subfields.count(subfield));
-  }
-
-  string DuckDBEnginePlan::RemoveExtension(string &function_name) {
-    // Lets first drop any extension id
+  string RemoveExtension(string &function_name) {
     string name;
+
     for (auto &c : function_name) {
       if (c == ':') { break; }
       name += c;
@@ -90,32 +56,135 @@ namespace duckdb {
     return name;
   }
 
-  string DuckDBEnginePlan::RemapFunctionName(string &function_name) {
+  string RemapFunctionName(string &function_name) {
     string name { RemoveExtension(function_name) };
 
-    auto it = function_names_remap.find(name);
-    if (it != function_names_remap.end()) { name = it->second; }
+    auto it = engine_remapped_functions.find(name);
+    if (it != engine_remapped_functions.end()) { name = it->second; }
 
     return name;
   }
 
-  string DuckDBEnginePlan::FindFunction(uint64_t id) {
-    if (functions_map.find(id) == functions_map.end()) {
-      throw InternalException("Could not find aggregate function " + to_string(id));
-    }
 
-    return functions_map[id];
+  // >> Static data and related functions for extraction of date subfields
+
+  static case_insensitive_set_t engine_date_subfields {
+     "year"       , "month"       , "day"
+    ,"decade"     , "century"     , "millenium"
+    ,"quarter"
+    ,"microsecond", "milliseconds", "second"
+    ,"minute"     , "hour"
+  };
+
+  void AssertValidDateSubfield(const string& subfield) {
+    D_ASSERT(engine_date_subfields.count(subfield));
   }
 
-  shared_ptr<Relation>
-  DuckDBEnginePlan::EnginePlanFromSubstraitPlan(substrait::Plan& plan) {
-    if (plan.relations().empty()) {
-      throw InvalidInputException("Substrait Plan does not have a SELECT statement");
-    }
+} // namespace: duckdb
 
-    RegisterExtensionFunctions(plan);
 
-    return TranslateRootOp(plan.relations(0).root());
+namespace duckdb {
+
+  //! Constructor for DuckDBTranslator
+  DuckDBTranslator::DuckDBTranslator(ClientContext &ctxt): context(ctxt) {
+    t_conn        = make_uniq<Connection>(*ctxt.db);
+    functions_map = make_uniq<mohair::SubstraitFunctionMap>();
+
+    // create an http state, but I don't know what this is for
+    auto http_state = HTTPState::TryGetState(*(t_conn->context));
+    http_state->Reset();
   }
 
-} // namespace duckdb
+  // >> Entry points for substrait plan (json or binary) -> duckdb logical plan
+  shared_ptr<DuckLogicalPlan>
+  DuckDBTranslator::TranspilePlanMessage(shared_ptr<DuckSystemPlan> sys_plan) {
+    shared_ptr<Relation> plan_rel = sys_plan->engine;
+
+    // Transform Relation to QueryNode and wrap in a SQLStatement
+    auto plan_wrapper  = make_uniq<SelectStatement>();
+    plan_wrapper->node = plan_rel->GetQueryNode();
+
+    // Create a planner to go from SQLStatement -> LogicalOperator
+    Planner planner { context };
+    planner.CreatePlan(std::move(plan_wrapper));
+    shared_ptr<LogicalOperator> logical_plan { std::move(planner.plan) };
+
+    return make_shared<DuckLogicalPlan>(sys_plan->substrait, logical_plan);
+  }
+
+  shared_ptr<DuckPhysicalPlan>
+  DuckDBTranslator::TranslateLogicalPlan( shared_ptr<DuckLogicalPlan> engine_plan
+                                         ,bool                        optimize) {
+    // Make a copy that is a unique_ptr
+    auto logical_plan = engine_plan->engine->Copy(context);
+
+    // optimization
+    if (optimize) {
+      shared_ptr<Binder> binder    { Binder::CreateBinder(context) };
+      Optimizer          optimizer { *binder, context };
+
+      logical_plan = optimizer.Optimize(std::move(logical_plan));
+    }
+
+    // transformation to physical plan
+    PhysicalPlanGenerator physical_planner { context };
+    shared_ptr<PhysicalOperator> physical_plan {
+      physical_planner.CreatePlan(std::move(logical_plan))
+    };
+
+    return make_shared<DuckPhysicalPlan>(engine_plan->substrait, physical_plan);
+  }
+
+  bool ShouldKeepExecuting(PendingExecutionResult& exec_result) {
+    switch (exec_result) {
+      case PendingExecutionResult::RESULT_NOT_READY:
+      case PendingExecutionResult::RESULT_READY:
+        break;
+
+      case PendingExecutionResult::BLOCKED:
+        std::cout << "\t[Executor]: blocked" << std::endl;
+        break;
+
+      case PendingExecutionResult::NO_TASKS_AVAILABLE:
+        std::cout << "\t[Executor]: waiting for tasks" << std::endl;
+        break;
+
+      case PendingExecutionResult::EXECUTION_ERROR:
+        std::cerr << "\t[Executor]: execution error" << std::endl;
+        return false;
+
+      default:
+        std::cerr << "\t[Executor]: unknown execution result type" << std::endl;
+        return false;
+    }
+
+    return true;
+  }
+
+  unique_ptr<QueryResult> DuckDBExecutor::Execute() {
+    constexpr bool dry_run       { false };
+    Executor       plan_executor { context };
+
+    plan_executor.Initialize(
+      PhysicalResultCollector::GetResultCollector(context, plan_data)
+    );
+
+    auto exec_result = plan_executor.ExecuteTask(dry_run);
+    while (exec_result != PendingExecutionResult::RESULT_READY) {
+      if (not ShouldKeepExecuting(exec_result)) {
+        std::cerr << "\t\t" << plan_executor.GetError().Message() << std::endl;
+        break;
+      }
+
+      exec_result = plan_executor.ExecuteTask(dry_run);
+    }
+
+    if (    exec_result == PendingExecutionResult::RESULT_READY
+        and plan_executor.HasResultCollector()) {
+      return std::move(plan_executor.GetResult());
+    }
+
+    return nullptr;
+  }
+
+} // namespace: duckdb
