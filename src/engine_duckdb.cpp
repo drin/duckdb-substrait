002b465a8c87b7feb90202628c213a10cb1b2f36
Aldrin Montana <octalene.dev@pm.me>
2024-12-17 20:00:27 -0800

    task: updates to change substrait execution path
    Instead of directly executing the duckdb::Relation constructed from the
    substrait plan, it is translated to LogicalOperator then
    PhysicalOperator then executed with a `duckdb::Executor`. The purpose of
    this is to be able to always return a pushback plan instead of just a
    ticket. This is necessary for cooperative decomposition.

--- src/engine_duckdb.cpp
+++ src/engine_duckdb.cpp
@@ -31,7 +31,7 @@ namespace duckdb {
 
   // >> Static data and related functions for mapping functions from substrait -> duckdb
 
-  static FunctionRenameMap engine_remapped_functions {
+  static std::unordered_map<string, string> engine_remapped_functions {
      {"modulus"    , "mod"      }
     ,{"std_dev"    , "stddev"   }
     ,{"starts_with", "prefix"   }
@@ -92,93 +92,82 @@ namespace duckdb {
   }
 
   // >> Entry points for substrait plan (json or binary) -> duckdb logical plan
-  shared_ptr<DuckLogicalPlan>
-  DuckDBTranslator::TranspilePlanMessage(shared_ptr<DuckSystemPlan> sys_plan) {
-    shared_ptr<Relation> plan_rel = sys_plan->engine;
-
+  unique_ptr<LogicalOperator>
+  DuckDBTranslator::TranspilePlanMessage(Relation& plan_rel) {
     // Transform Relation to QueryNode and wrap in a SQLStatement
     auto plan_wrapper  = make_uniq<SelectStatement>();
-    plan_wrapper->node = plan_rel->GetQueryNode();
+    plan_wrapper->node = plan_rel.GetQueryNode();
 
     // Create a planner to go from SQLStatement -> LogicalOperator
     Planner planner { context };
     planner.CreatePlan(std::move(plan_wrapper));
-    shared_ptr<LogicalOperator> logical_plan { std::move(planner.plan) };
 
-    return make_shared_ptr<DuckLogicalPlan>(sys_plan->substrait, logical_plan);
+    return std::move(planner.plan);
   }
 
-  shared_ptr<DuckPhysicalPlan>
-  DuckDBTranslator::TranslateLogicalPlan( shared_ptr<DuckLogicalPlan> engine_plan
-                                         ,bool                        optimize) {
+  unique_ptr<PhysicalOperator>
+  DuckDBTranslator::TranslateLogicalPlan(LogicalOperator& logical_plan, bool optimize) {
     // Make a copy that is a unique_ptr
-    auto logical_plan = engine_plan->engine->Copy(context);
+    auto engine_plan = logical_plan.Copy(context);
 
     // optimization
     if (optimize) {
       shared_ptr<Binder> binder    { Binder::CreateBinder(context) };
       Optimizer          optimizer { *binder, context };
 
-      logical_plan = optimizer.Optimize(std::move(logical_plan));
+      engine_plan = optimizer.Optimize(std::move(engine_plan));
     }
 
     // transformation to physical plan
     PhysicalPlanGenerator physical_planner { context };
-    shared_ptr<PhysicalOperator> physical_plan {
-      physical_planner.CreatePlan(std::move(logical_plan))
-    };
-
-    return make_shared_ptr<DuckPhysicalPlan>(engine_plan->substrait, physical_plan);
+    return physical_planner.CreatePlan(std::move(engine_plan));
   }
 
-  bool ShouldKeepExecuting(PendingExecutionResult& exec_result) {
+  bool ExecutionFailed(PendingExecutionResult& exec_result) {
     switch (exec_result) {
       case PendingExecutionResult::RESULT_NOT_READY:
       case PendingExecutionResult::RESULT_READY:
+      case PendingExecutionResult::EXECUTION_FINISHED:
         break;
 
       case PendingExecutionResult::BLOCKED:
-        std::cout << "\t[Executor]: blocked" << std::endl;
-        break;
-
       case PendingExecutionResult::NO_TASKS_AVAILABLE:
-        std::cout << "\t[Executor]: waiting for tasks" << std::endl;
+        // std::cout << "\t[Executor]: blocked or waiting" << std::endl;
         break;
 
       case PendingExecutionResult::EXECUTION_ERROR:
         std::cerr << "\t[Executor]: execution error" << std::endl;
-        return false;
+        return true;
 
       default:
         std::cerr << "\t[Executor]: unknown execution result type" << std::endl;
-        return false;
+        return true;
     }
 
-    return true;
+    return false;
   }
 
   unique_ptr<QueryResult> DuckDBExecutor::Execute() {
-    constexpr bool dry_run       { false };
-    Executor       plan_executor { context };
-
+    Executor plan_executor { context };
     plan_executor.Initialize(
       PhysicalResultCollector::GetResultCollector(context, plan_data)
     );
 
+    constexpr bool dry_run    { false };
+    size_t         task_count { 1     };
+
     auto exec_result = plan_executor.ExecuteTask(dry_run);
-    while (exec_result != PendingExecutionResult::RESULT_READY) {
-      if (not ShouldKeepExecuting(exec_result)) {
+    while (exec_result != PendingExecutionResult::EXECUTION_FINISHED) {
+      if (ExecutionFailed(exec_result)) {
         std::cerr << "\t\t" << plan_executor.GetError().Message() << std::endl;
-        break;
+        return nullptr;
       }
 
+      ++task_count;
       exec_result = plan_executor.ExecuteTask(dry_run);
     }
 
-    if (    exec_result == PendingExecutionResult::RESULT_READY
-        and plan_executor.HasResultCollector()) {
-      return plan_executor.GetResult();
-    }
+    if (plan_executor.HasResultCollector()) { return plan_executor.GetResult(); }
 
     return nullptr;
   }
